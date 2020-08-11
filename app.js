0054ae51822ebe0e05d26f088cfe96ec9148ec9f
Abdulrahman Ibrahim <eng.ibrahim1001@gmail.com>
2020-08-11 15:21:11 +0200

    new1

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 //================== Require area===============================//
 const express = require('express')
 const app = express()
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 //======================== end Require area====================================//
 //======================== Routs area=====================================//
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render('index');
+});
+app.get('/shop', (req, res) => {
+    res.render('shop')
 });
 //========================end  Routs area=====================================//
 
