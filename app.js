9488851c3ca15fcff22e98c56854672725ca06c2
Bashar <bsalman395850@gmail.com>
2020-08-11 14:24:54 +0200

    npm_i_package

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+//================== Require area===============================//
 const express = require('express')
 const app = express()
+//======================== end Require area====================================//
+//======================== Routs area=====================================//
 app.get('/', (req, res) => {
     res.send('hello');
 });
-app.get('/', (req, res) => {
- res.send('bla')
-})
+//========================end  Routs area=====================================//
+
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
