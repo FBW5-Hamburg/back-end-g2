5e129340be93aca8e61ac28ad903fbf153e6c586
Bashar <bsalman395850@gmail.com>
2020-08-12 10:01:31 +0200

    bsahar_12_8_2020

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 //================== Require area===============================//
 const express = require('express')
 const app = express()
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 //======================== end Require area====================================//
 //======================== Routs area=====================================//
+
+//=================================0//
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render('index')
 });
+
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+})
 //========================end  Routs area=====================================//
 
 app.listen(3000, () => {
