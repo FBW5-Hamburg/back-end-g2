108b36946ed0f4e31a819c060bbe3e0a9f85fdd7
Bashar <bsalman395850@gmail.com>
2020-08-13 10:20:26 +0200

    bashar_login_13_8_2020

--- app.js
+++ app.js
@@ -20,13 +20,20 @@ app.get('/', (req, res) => {
 app.get('/contact',(req,res)=>{
     res.render('contact')
 })
-
+//=======================shop ============================//
+app.get('/shop',(req,res)=>{
+    res.render('shop')
+})
+//=====================shop_cart===========================//
+app.get('/shopCart',(req,res)=>{
+    res.render('shopCart')
+})
+//==================== register===========================//
 app.get('/register',(req,res)=>{
     res.render('register')
 })
 app.post('/register',(req,res)=>{
-
-    console.log(req.body);
+    // console.log(req.body);
     const email=req.body.email.trim();
     const password =req.body.password;
     const rePassword= req.body.password;
@@ -45,7 +52,34 @@ app.post('/register',(req,res)=>{
         res.json(2)
     }
 
+})
+//======================== login================================//
+//1=success or exist 
+//2= missing entry
+//3=not exist
+//4=server problem
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+app.post('/login',(req,res)=>{
+    // console.log(req.body);
+    const logInEmail= req.body.email.trim();
+    const logInPassword= req.body.password.trim();
 
+    if(logInEmail&& logInPassword){
+        dataModule.checkCustomer(logInEmail, logInPassword).then((customer)=>{
+           res.json(1)
+})
+       .catch((error)=>{
+           if (error==3) {
+               res.json(3)
+           }else{
+            res.json(4)
+           }
+       })
+   }else{
+    res.json(2)
+   }
 
     
 })
--- modules/mongooseDataModule.js
+++ modules/mongooseDataModule.js
@@ -1,50 +1,31 @@
 //===================== Require Area===========================//
 //password hash
-const passwordHash= require('password-hash')
+const passwordHash = require('password-hash')
 //file system 
 const fs = require('fs')
 // require Mongoose
 const mongoose = require('mongoose')
-//
-const connectionString ='mongodb+srv://user1:MnZd6whfj08ESEh7@cluster0.jufz4.mongodb.net/test1?retryWrites=true&w=majority'
+// getting connection string from data base
+const connectionString = 'mongodb+srv://user1:MnZd6whfj08ESEh7@cluster0.jufz4.mongodb.net/test1?retryWrites=true&w=majority'
 
 const Schema = mongoose.Schema
 
 //  creating Schema
-const customerSchema = new Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+const customersSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String, 
-        required:true
+    password: {
+        type: String,
+        required: true
     }
 })
 //
-const Customer = mongoose.model('customer', customerSchema)
+const Customers = mongoose.model('customers', customersSchema)
 //=====================  end Require Area===========================//
 //==================== function area========================//
-//connect function ,
-// function dataBaseConnect() {
-//     return new Promise ((resolve,reject)=>{
-//         if (mongoose.connection.readyState===1) {
-//             resolve()
-//         }else{
-//             mongoose.dataBaseConnect( connectionString,{
-//                 useUnifiedTopology: true,
-//                 useCreateIndex: true,
-//                 useNewUrlParser: true
-//             }).then(()=>{
-//                 resolve()
-//             }).catch((error)=>{
-//                 reject(error)
-//             })
-//         }
-//     })
-    
-// }
 function connect() {
     return new Promise((resolve, reject) => {
         if (mongoose.connection.readyState === 1) {
@@ -64,17 +45,17 @@ function connect() {
     })
 }
 
-// register Function 
-function registerCustomer(email,password) {
-    return new Promise((resolve,reject)=>{
-        connect().then(()=>{
-            const newCustomer = new Customer ({
-                email:email,
-                password:passwordHash.generate(password)
+// register customer and creat an account  
+function registerCustomer(email, password) {
+    return new Promise((resolve, reject) => {
+        connect().then(() => {
+            const newCustomers = new Customers({
+                email: email,
+                password: passwordHash.generate(password)
             })
-            newCustomer.save().then((result)=>{
-               resolve(result)
-            }).catch(error=>{
+            newCustomers.save().then((result) => {
+                resolve(result)
+            }).catch(error => {
                 console.log(error);
                 if (error.code === 11000) {
                     reject('exist');
@@ -82,13 +63,45 @@ function registerCustomer(email,password) {
                     reject(error);
                 }
             })
-        }).catch((error)=>{
+        }).catch((error) => {
             reject(error)
         })
     })
-    
+
 }
-module.exports={
-  
-    registerCustomer
+// check if the customer has an account in the Data base
+function checkCustomer(email,password) {
+     return new Promise((resolve, reject) => {
+       connect().then(() => {
+        Customers.findOne({
+                email: email
+            }).then((result) => {
+                 console.log(result);
+                if (result) {
+                    if (passwordHash.verify(password, result.password)) {
+                        //exist
+                        resolve(result)
+                    } else {
+                        //not exist
+                        reject(3)
+   }
+                } else {
+                    reject(3)
+                }
+            }).catch(error => {
+                console.log(error);
+                reject(error)
+            })
+        }).catch((error) => {
+            console.log(error);
+            reject(error)
+      })
+    })
+
+ }
+
+module.exports = {
+
+    registerCustomer,
+    checkCustomer
 }
