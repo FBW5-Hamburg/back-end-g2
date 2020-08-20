590200ad615342528be06302b2ed0479d816efbd
Bashar <bsalman395850@gmail.com>
2020-08-20 14:28:54 +0200

    fix adminRout

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,77 +2,80 @@ const express = require('express')
 const dataModule = require('../modules/mongooseDataModule')
 const adminRouter = express.Router()
 
-
-
-adminRouter.use((req, res ,next) => {
+adminRouter.use((req, res, next) => {
     if (req.session.user) {
-     if (req.session.user.role === 'admin'){
-         next()
-        }else {res.redirect('/login') }
 
-              
-    
+        if (req.session.user.role === 'admin') {
+            next()
+        } else {
+            res.redirect('/login')
+        }
+    } else {
+        res.redirect('/login')
+    }
+})
 
 adminRouter.get('/', (req, res) => {
-    res.render('admin',{login:req.session.user})
-    console.log(req.session.user
-        );
+    res.render('admin', {
+        login: req.session.user
+    })
+    console.log(req.session.user);
 })
 adminRouter.get('/addproducts', (req, res) => {
-    res.render('addproducts',{login:req.session.user});
+    res.render('addproducts', {
+        login: req.session.user
+    });
 })
 
 adminRouter.post('/addproducts', (req, res) => {
-
-
-    console.log(req.body);
-    console.log(req.files);
+    //console.log(req.body);
+    //console.log(req.files);
     // responses map
     // 1 book saved successfuly
     // 2 data error
-//console.log(req.body);
-console.log(Object.keys( req.files));
-if (req.files) {
+    //console.log(req.body);
+    //console.log(Object.keys(req.files));
+    if (req.files) {
 
 
-const productName = req.body.productName
-const productDescription = req.body.productDescription
-const productCategories = req.body.productCategories
-const productColor = req.body.productColor
-const productPrice = req.body.productPrice
-const productSize = req.body.productSize
+        const productName = req.body.productName
+        const productDescription = req.body.productDescription
+        const productCategories = req.body.productCategories
+        const productColor = req.body.productColor
+        const productPrice = req.body.productPrice
+        const productSize = req.body.productSize
 
 
-if (productName && productDescription && productCategories && productColor && productPrice && productSize && Object.keys( req.files).length > 1){
-    const imgs = []
-    for (const key in req.files) {
-        if (req.files[key].mimetype == 'image/jpeg') {
-            imgs.push(req.files[key])
+        if (productName && productDescription && productCategories && productColor && productPrice && productSize && Object.keys(req.files).length > 1) {
+            const imgs = []
+            for (const key in req.files) {
+                if (req.files[key].mimetype == 'image/jpeg') {
+                    imgs.push(req.files[key])
+                }
+            }
+            dataModule.addProduct(productName, productDescription, productCategories, productColor, productPrice, productSize, imgs, req.session.user._id).then(() => {
+                res.json(1)
+            }).catch(error => {
+                if (error == 3) {
+                    res.json(3)
+                }
+            })
+
+        } else {
+            res.json(2)
         }
-
-    dataModule.addProduct(productName, productDescription, productCategories, productColor, productPrice, productSize, req.session.userid ).then(() => {
-        res.json(1)
-    }).catch(error => {
-        if (error == 3) {
-            res.json(3)
-        }
-    })
-
-} else {
-    res.json(2)
-}
-} else {
-    res.json(2)
-}
-
+    } else {
+        res.json(2)
+    }
 
 })
 
-//=============================================//
-
+//==============================================================//
 adminRouter.get('/myproducts', (req, res) => {
     dataModule.userProducts(req.session.user._id).then(products => {
-        res.render('myproducts', {products})
+        res.render('myproducts', {
+            products
+        })
     }).catch(error => {
         res.send('404. page not found')
     })
